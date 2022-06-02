a13ebfcfca9633785549220eeee237d73120c254
SalenaLuu <codingseahorse@outlook.de>
2022-06-02 22:40:10 +0200

    created a BlogPostApplicationTest

--- blog-post/src/test/java/com/salenaluu/portfolio/blogpost/BlogPostApplicationTests.java
+++ blog-post/src/test/java/com/salenaluu/portfolio/blogpost/BlogPostApplicationTests.java
@@ -3,11 +3,13 @@ package com.salenaluu.portfolio.blogpost;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 class BlogPostApplicationTests {
-
 	@Test
 	void contextLoads() {
+		BlogPostApplication blogPostApplication = new BlogPostApplication();
+		assertThat(blogPostApplication).isNotNull();
 	}
-
 }
